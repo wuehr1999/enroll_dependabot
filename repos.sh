e2927891d763c5f00e5a44c8fa64bca9f0a2767d
wuehr1999 <jonas.wuehr@t-online.de>
2022-10-17 09:57:12 +0200

    fix issues

--- repos.sh
+++ repos.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # PLACE YOUR REPOSITORY URLS IN THIS ARRAY
-REPOS=(git@github.com:wuehr1999/sandbox.git)
+REPOS=(git@github.com:wuehr1999/sandbox.git git@github.com:wuehr1999/sandbox2.git)
